6f2e5e5b1023c13a82bac499e8a6568243b49b26
Sid-0906 <sm5094@srmist.edu.in>
2026-03-02 22:50:24 +0530

    Implemented UC4-printoops

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,102 +1,18 @@
 public class oopsbannerapp {
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     public static void main(String[] args) {
-
-        System.out.println(String.join("",
-                " *****   ",
-                " *****   ",
-                " ******  ",
-                " *****"
-        ));
-
-        System.out.println(String.join("",
-                "*     *  ",
-                "*     *  ",
-                "*     *  ",
-                "*"
-        ));
-
-        System.out.println(String.join("",
-                "*     *  ",
-                "*     *  ",
-                "*     *  ",
-                "*"
-        ));
-
-        System.out.println(String.join("",
-                "*     *  ",
-                "*     *  ",
-                "******   ",
-                " ****"
-        ));
-
-        System.out.println(String.join("",
-                "*     *  ",
-                "*     *  ",
-                "*        ",
-                "     *"
-        ));
-
-        System.out.println(String.join("",
-                "*     *  ",
-                "*     *  ",
-                "*        ",
-                "     *"
-        ));
-
-        System.out.println(String.join("",
-                " *****   ",
-                " *****   ",
-                "*        ",
-                "*****"
-        ));
-    }
-=======
-    // Method to print banner
-    public static void printBanner() {
 
         // Store banner lines in a String array
         String[] bannerLines = {
-            "=======================",
-            "        OOPS           ",
-            " Object Oriented       ",
-            " Programming System    ",
-            "======================="
+            " *****    *****    *****     ***** ",
+            "*     *  *     *  *     *   *      ",
+            "*     *  *     *  *****     *****  ",
+            "*     *  *     *  *              *  ",
+            " *****    *****   *         *****"
         };
 
-        // Loop through the array and print each line
-        for (int i = 0; i < bannerLines.length; i++) {
-            System.out.println(bannerLines[i]);
+        // Print banner using a loop
+        for (String line : bannerLines) {
+            System.out.println(line);
         }
     }
-=======
-    // Method to print banner
-    public static void printBanner() {
-
-        // Store banner lines in a String array
-        String[] bannerLines = {
-            "=======================",
-            "        OOPS           ",
-            " Object Oriented       ",
-            " Programming System    ",
-            "======================="
-        };
-
-        // Loop through the array and print each line
-        for (int i = 0; i < bannerLines.length; i++) {
-            System.out.println(bannerLines[i]);
-        }
-    }
->>>>>>> Stashed changes
-
-    // Main method
-    public static void main(String[] args) {
-        printBanner();  // Calling the method
-    }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
