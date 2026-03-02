2ace25594d1882eba9fc0526ebef8e4a84e99cb5
Sid-0906 <sm5094@srmist.edu.in>
2026-03-02 22:36:27 +0530

    Extended UC3 with String array and loop

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,6 +1,7 @@
 public class oopsbannerapp {
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public static void main(String[] args) {
 
         System.out.println(String.join("",
@@ -70,10 +71,32 @@ public class oopsbannerapp {
             System.out.println(bannerLines[i]);
         }
     }
+=======
+    // Method to print banner
+    public static void printBanner() {
+
+        // Store banner lines in a String array
+        String[] bannerLines = {
+            "=======================",
+            "        OOPS           ",
+            " Object Oriented       ",
+            " Programming System    ",
+            "======================="
+        };
+
+        // Loop through the array and print each line
+        for (int i = 0; i < bannerLines.length; i++) {
+            System.out.println(bannerLines[i]);
+        }
+    }
+>>>>>>> Stashed changes
 
     // Main method
     public static void main(String[] args) {
         printBanner();  // Calling the method
     }
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 }
